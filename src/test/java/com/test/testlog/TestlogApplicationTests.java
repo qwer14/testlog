65ad0313e8f2d4d6241dd0b019b9a9b98a4a7797
Qwer14 <987881416@qq.com>
2020-12-11 16:56:32 +0800

    测试

---
+++ src/test/java/com/test/testlog/TestlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.testlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
